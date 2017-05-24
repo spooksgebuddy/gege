81991147a1f57894f5272db0c73bf6ae8d1be6d2
spooksgebuddy <ihalfside@gmail.com>
2017-05-24 23:55:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -294,7 +294,7 @@
           $(function () {
       			$('#container').highcharts(options,
       				function (chart) { // on complete
-              chart.renderer.image('https://rsbuddy.com/items/'+getItem(item,"id")+'.png', 80, 15, 36, 32).add();
+              chart.renderer.image('https://rsbuddy.com/items/'+getItem(item,"id")+'.png', 85, 15, 36, 32).add();
               chart.setTitle({ text: item}, { useHTML: true, text: 'Current price: '+ numberWithCommas(currentPrice) + ' gp | ' + '<a href="//www.ge-tracker.com/item/' + getItem(item,"id") + '" target="_blank" >Ge-Tracker</a>'+ ' | ' + '<a href="//docs.google.com/spreadsheets/d/1NXXcnCI8p5Jp1ylF-uNH-oNTn_oFbNn11_DUlHDGDCw/edit?usp=sharing" target="_blank" >Compu</a>' }
                             );
       					});

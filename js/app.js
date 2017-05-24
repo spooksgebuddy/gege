9708f6e9ec3c144b212aab790c88587d9d314722
spooksgebuddy <ihalfside@gmail.com>
2017-05-24 22:18:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -294,7 +294,7 @@
           $(function () {
       			$('#container').highcharts(options,
       				function (chart) { // on complete
-              chart.setTitle({ text: item}, { useHTML: true, text: 'Current price: '+ numberWithCommas(currentPrice) + ' gp | ' + '<a href="//www.ge-tracker.com/item/' + getItem(item,"id") + '" target="_blank" >Ge-Tracker</a>' }
+              chart.setTitle({ text: item}, { useHTML: true, text: 'Current price: '+ numberWithCommas(currentPrice) + ' gp | ' + '<a href="//www.ge-tracker.com/item/' + getItem(item,"id") + '" target="_blank" >Ge-Tracker</a>'+ ' | ' + '<a href="//docs.google.com/spreadsheets/d/1NXXcnCI8p5Jp1ylF-uNH-oNTn_oFbNn11_DUlHDGDCw/edit?usp=sharing" target="_blank" >Compu</a>' }
                             );
       					});
       			Highcharts.setOptions({
